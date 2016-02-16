30f6ee0a072a5f7e7ed2ec24b593a6ad2e17a0f9
高橋翼 <tsubasatakahashi47@gmail.com>
2016-02-16 17:09:53 +0800

    Initial Commit

---
+++ myApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myApp
+//
+//  Created by Tsubasa Takahashi on 2016/02/16.
+//  Copyright © 2016年 Tsubasa Takahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
